6ba06682fe5cf98cfe656cffdf0a049d52055081
Sayan Chatterjee <47378467+chatterjeesayan1992@users.noreply.github.com>
2026-05-13 20:51:44 +0530

    Add: permutation-string - submission-4

---
+++ Data Structures & Algorithms/permutation-string/submission-4.py
@@ -0,0 +1,59 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        # from collections import Counter
+        # l,r = 0, len(s1)
+        # count_s1 = Counter(s1).items()
+
+        # while r < len(s2) +1 :
+        #     substr = s2[l:r]
+        #     count_substr = Counter(substr).items()         
+
+        #     if count_s1 == count_substr:
+        #         return True
+        #     else:
+        #         r += 1
+        #         l += 1
+        
+        # return False  
+
+        if len(s1) > len(s2):
+            return False
+
+        count_s1, count_s2 = [0]*26, [0]*26
+
+        for i in range(len(s1)):
+            count_s1[ord(s1[i]) - ord('a')] += 1
+            count_s2[ord(s2[i]) - ord('a')] += 1
+        
+        matches = 0
+        for i in range(26):
+            matches += (1 if count_s1[i] == count_s2[i] else 0)
+        
+        l = 0
+        for r in range(len(s1),len(s2)):
+            if matches == 26:
+                return True
+            
+            index = ord(s2[r]) - ord('a')
+            count_s2[index] += 1
+            if count_s2[index] == count_s1[index]:
+                matches += 1
+            elif count_s1[index] + 1 == count_s2[index]:
+                matches -=1
+            
+            
+            index = ord(s2[l]) - ord('a')
+            count_s2[index] -=1
+            if count_s2[index] == count_s1[index]:
+                matches += 1
+            elif count_s1[index] - 1 == count_s2[index]:
+                matches -= 1
+            l += 1
+        
+        return matches == 26
+            
+    
+
+
+     
+        
